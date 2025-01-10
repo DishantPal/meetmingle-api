6b465bd860f970cdcdcfd5b1dd66f2a66ebd4f34
Dishant Pal <dspal@enacton.com>
2025-01-10 11:00:45 +0530

    added settings for filter price

---
+++ src/database/seeds/1736486690380_add_filter_seetings_seeder.ts
@@ -0,0 +1,46 @@
+import type { Kysely } from 'kysely'
+
+export async function seed(db: Kysely<any>): Promise<void> {
+  const now = new Date()
+  console.log("🚀 ~ seed ~ now:", now)
+  
+  const settings = [
+    // Filter Settings
+    { 
+      key: 'age_filter_price', 
+      value: 10, 
+      group: 'filter' 
+    },
+    { 
+      key: 'gender_filter_price', 
+      value: 0, 
+      group: 'filter' 
+    },
+    { 
+      key: 'language_filter_price', 
+      value: 0, 
+      group: 'filter' 
+    },
+    { 
+      key: 'state_filter_price', 
+      value: 10, 
+      group: 'filter' 
+    },
+    { 
+      key: 'country_filter_price', 
+      value: 10, 
+      group: 'filter' 
+    },
+  ]
+
+  // Insert all settings
+  for (const setting of settings) {
+    await db
+      .insertInto('app_settings')
+      .values({
+        ...setting,
+        updated_at: now
+      })
+      .execute()
+  }
+}
